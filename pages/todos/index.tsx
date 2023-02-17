e7faeddfb7a4c36df23f026315ca945ae3f53031
mdubelbeis <mason.dubelbeis@gmail.com>
2023-02-17 11:16:10 -0600

    styled mobile nav

--- pages/todos/index.tsx
+++ pages/todos/index.tsx
@@ -1,7 +1,7 @@
 const TodosHomePage = () => {
   return (
-    <div className="relative h-screen w-screen flex gap-1">
-      <aside className="relative h-full w-40 bg-slate-600 text-center">
+    <div className="relative top-0 bottom-0 -left-40 h-screen w-screen flex">
+      <aside className="h-full w-40 bg-slate-600 text-center">
         <h1 className="text-xl p-2">My Todos</h1>
         <div>Favorited Notes</div>
 
@@ -11,13 +11,16 @@ const TodosHomePage = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Voluptatem, facilis?
             </li>
-            <li>Note 2</li>
-            <li>Note 3</li>
-            <li>Note 4</li>
-            <li>Note 5</li>
+            <li>Lorem amet consectir</li>
+            <li>ispusum odispe odipie gite</li>
+            <li>itufe e</li>
+            <li>piesrt tirst t</li>
           </ul>
         </div>
       </aside>
+      <div className="fixed left-0 top-[50%] -translate-y-[50%] flex items-center h-20 bg-slate-500 p-3 ">
+        <span className="place-self-center text-white">{">"}</span>
+      </div>
       <main className="h-full w-full">TODO</main>
     </div>
   );
